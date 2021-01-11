5886ec32334a5a2e4f242ae2a64a87af3076eed6
jinder1s <msingh@edx.org>
2021-01-11 15:34:11 -0500

    Changed url to point to updated specification context

--- event_routing_backends/processors/caliper/constants.py
+++ event_routing_backends/processors/caliper/constants.py
@@ -2,4 +2,4 @@
 Constants related to IMS Caliper and events transformation into Caliper.
 """
 
-CALIPER_EVENT_CONTEXT = 'http://purl.imsglobal.org/ctx/caliper/v1p1'
+CALIPER_EVENT_CONTEXT = 'http://purl.imsglobal.org/ctx/caliper/v1p2'
