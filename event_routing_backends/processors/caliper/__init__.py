487ebc6b76c108e0476da42c8a21e076bfec9d27
Tim McCormack <tmccormack@edx.org>
2021-02-08 15:22:41 +0000

    doc: Indicate that events toggles are permanent (#49)
    We'll want to keep these toggles because eventing is going to place
    different loads on the system, and we may need to be able to shut off
    external eventing in a coarse-grained way due to operational emergencies.
    
    Also, fix creation date of toggles.

--- event_routing_backends/processors/caliper/__init__.py
+++ event_routing_backends/processors/caliper/__init__.py
@@ -9,12 +9,13 @@ from edx_toggles.toggles import SettingToggle
 # .. toggle_implementation: SettingToggle
 # .. toggle_default: False
 # .. toggle_description: Allow sending events to external servers via Caliper.
+#   Toggle intended both for gating initial release and for shutting off all
+#   Caliper events in case of emergency.
 # .. toggle_warnings: Do not enable sending of Caliper events until there has
 #   been a thorough review of PII implications and safeguards put in place to
 #   prevent accidental leakage of novel event fields to third parties. See
 #   ARCHBOM-1655 for details.
-# .. toggle_use_cases: temporary
-# .. toggle_creation_date: 2020-01-01
-# .. toggle_target_removal_date: 2021-07-01
+# .. toggle_use_cases: circuit_breaker
+# .. toggle_creation_date: 2021-01-01
 # .. toggle_tickets: https://openedx.atlassian.net/browse/ARCHBOM-1658
 CALIPER_EVENTS_ENABLED = SettingToggle("CALIPER_EVENTS_ENABLED", default=False)
