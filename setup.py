dc0e320c08dc5350b0c4b8e528ec20d9a2aead9f
Jawayria <39649635+Jawayria@users.noreply.github.com>
2021-07-13 21:24:27 +0500

    Update setup.py
    Co-authored-by: Usama Sadiq <usama.sadiq@arbisoft.com>

--- setup.py
+++ setup.py
@@ -86,6 +86,7 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
         'Framework :: Django :: 3.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
