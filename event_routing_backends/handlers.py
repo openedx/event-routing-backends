a0c66c162f983af5cb6a5cce1874771644573c70
Cristhian Garcia <crisgarta8@gmail.com>
2023-10-26 16:58:42 -0500

    chore: quality fixes

--- event_routing_backends/handlers.py
+++ event_routing_backends/handlers.py
@@ -5,19 +5,19 @@ import json
 import logging
 
 from django.dispatch import receiver
+from eventtracking.backends.event_bus import EventBusRoutingBackend
 from eventtracking.processors.exceptions import EventEmissionExit
 from eventtracking.tasks import send_event
 from eventtracking.tracker import get_tracker
 from openedx_events.analytics.signals import TRACKING_EVENT_EMITTED
 
-
-from eventtracking.backends.event_bus import EventBusRoutingBackend
-
 logger = logging.getLogger(__name__)
 
 
 @receiver(TRACKING_EVENT_EMITTED)
-def send_tracking_log_to_backends(sender, signal, **kwargs):
+def send_tracking_log_to_backends(
+    sender, signal, **kwargs
+):  # pylint: disable=unused-argument
     """
     Listen for the TRACKING_EVENT_EMITTED signal and send the event to the enabled backend.
     """
@@ -32,12 +32,16 @@ def send_tracking_log_to_backends(sender, signal, **kwargs):
 
     tracker = get_tracker()
 
-    engines = {name: engine for name,engine in tracker.backends.items() if isinstance(engine, EventBusRoutingBackend)}
+    engines = {
+        name: engine
+        for name, engine in tracker.backends.items()
+        if isinstance(engine, EventBusRoutingBackend)
+    }
     for name, engine in engines.items():
         try:
             processed_event = engine.process_event(event)
             logger.info('Successfully processed event "{}"'.format(event["name"]))
+            send_event(name, processed_event, True)
         except EventEmissionExit:
             logger.info("[EventEmissionExit] skipping event {}".format(event["name"]))
             return
-        send_event(name, processed_event, raise_on_error=True)
