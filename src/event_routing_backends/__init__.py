83e5b5d05ac9596967edaa6c97d518def521af89
irfanuddinahmad <34648393+irfanuddinahmad@users.noreply.github.com>
2026-09-09 14:21:08 +0500

    fix: remove unnecessary try/except around __version__ (not present pre-migration)

--- src/event_routing_backends/__init__.py
+++ src/event_routing_backends/__init__.py
@@ -2,10 +2,6 @@
 Various backends for receiving edX LMS events..
 """
 
-from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as get_version
 
-try:
-    __version__ = get_version("edx-event-routing-backends")
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "0.0.0"
+__version__ = get_version("edx-event-routing-backends")
